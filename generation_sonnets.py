3879167b0f32ad35ec58644d2a66b6b4304e5810
Clément Plancq <clement.plancq@ens.fr>
2019-02-20 23:31:42 +0100

    Factorisation de code pour passage de schéma en argument

--- generation_sonnets.py
+++ generation_sonnets.py
@@ -5,46 +5,40 @@ import pickle
 
 types_rimes = pickle.load(open('types_rimes.pickle', 'rb'))
     
-def generate():    
+def generate(schema):    
     longueur = len(types_rimes)
+    rimes = dict()
 
     while True :
         try :
             choix_rimes=random.sample(range(longueur), 5)
-            liste_A=random.sample(range(len(types_rimes[choix_rimes[0]])), 4)
-            liste_B=random.sample(range(len(types_rimes[choix_rimes[1]])), 4)
-            liste_C=random.sample(range(len(types_rimes[choix_rimes[2]])), 2)
-            liste_D=random.sample(range(len(types_rimes[choix_rimes[3]])), 2)
-            liste_E=random.sample(range(len(types_rimes[choix_rimes[4]])), 2)
+            indexes_A=random.sample(range(len(types_rimes[choix_rimes[0]])), 4)
+            rimes['A'] = [types_rimes[choix_rimes[0]][index] for index in indexes_A]
+            indexes_B=random.sample(range(len(types_rimes[choix_rimes[1]])), 4)
+            rimes['B'] = [types_rimes[choix_rimes[1]][index] for index in indexes_B]
+            indexes_C=random.sample(range(len(types_rimes[choix_rimes[2]])), 2)
+            rimes['C'] = [types_rimes[choix_rimes[2]][index] for index in indexes_C]
+            indexes_D=random.sample(range(len(types_rimes[choix_rimes[3]])), 2)
+            rimes['D'] = [types_rimes[choix_rimes[3]][index] for index in indexes_D]
+            indexes_E=random.sample(range(len(types_rimes[choix_rimes[4]])), 2)
+            rimes['E'] = [types_rimes[choix_rimes[4]][index] for index in indexes_E]
             break
 
         except :
             continue
 
-    sonnet = [types_rimes[choix_rimes[0]][liste_A[0]],
-    types_rimes[choix_rimes[1]][liste_B[0]],
-    types_rimes[choix_rimes[1]][liste_B[1]],
-    types_rimes[choix_rimes[0]][liste_A[1]],
-    '',
-    types_rimes[choix_rimes[0]][liste_A[2]],
-    types_rimes[choix_rimes[1]][liste_B[2]],
-    types_rimes[choix_rimes[1]][liste_B[3]],
-    types_rimes[choix_rimes[0]][liste_A[3]],
-    '',
-    types_rimes[choix_rimes[2]][liste_C[0]],
-    types_rimes[choix_rimes[2]][liste_C[1]],
-    types_rimes[choix_rimes[3]][liste_D[0]],
-    '',
-    types_rimes[choix_rimes[4]][liste_E[0]],
-    types_rimes[choix_rimes[3]][liste_D[1]],
-    types_rimes[choix_rimes[4]][liste_E[1]]]
-
-    
+    sonnet = list()
+    for stanza in schema:
+        for letter in stanza:
+            sonnet.append(rimes[letter].pop())
+        sonnet.append('')
+   
     return sonnet
 
 
 def main():
-    sonnet = generate()
+    schema=(('A', 'B', 'B', 'A'), ('A', 'B', 'B', 'A'), ('C', 'C', 'D'), ('E', 'D', 'E'))
+    sonnet = generate(schema)
     for l in sonnet:
         print(l)
 
