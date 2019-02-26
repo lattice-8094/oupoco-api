6ce6ba2d5937f6c70cb9525589476b4f926be0f5
Clément Plancq <clement.plancq@ens.fr>
2019-02-26 18:18:54 +0100

    first draft with restplus

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import generation_sonnets
 import datetime
 
 from flask import Flask
+from flask_restplus import Api, Resource
+
 from flask import jsonify, request
 
 app = Flask(__name__)
+api = Api(app)
 
 schemas = {
     'sonnet_sicilien':('ABAB','ABAB','CDE','CDE'),
@@ -16,38 +19,45 @@ schemas = {
     'sonnet_queneau':('ABAB','ABAB','CCD','EDE')
     }
 
-@app.route("/new",  methods=['GET'])
-def new():
-    param_schema = request.args.get('schema', None)
-    if param_schema in schemas:
-        sonnet = generation_sonnets.generate(schemas[param_schema])
-    else:
-        sonnet = generation_sonnets.generate()
-    sonnet_text = list()
-    for st in sonnet:
-        for verse in st:
-            sonnet_text.append({'text': verse['text'], 'meta': format_meta(verse['meta'])})
-        sonnet_text.append({'text': "\n", 'meta': ''})
-    res = {'text': sonnet_text, 'date': get_date_time()}
-    return jsonify(res)
 
-@app.route("/new-html",  methods=['GET'])
-def new_html():
-    param_schema = request.args.get('schema', None)
-    if param_schema in schemas:
-        sonnet = generation_sonnets.generate(schemas[param_schema])
-    else:
+@api.route("/new")
+@api.doc(params={'schema': 'The name of a schema (i.e. sonnet_francais)'})
+class New(Resource):
+    @api.doc(id='Returns a new sonnet in JSON')
+    def get(self):
+        param_schema = request.args.get('schema', None)
+        if param_schema in schemas:
+            sonnet = generation_sonnets.generate(schemas[param_schema])
+        else:
+            sonnet = generation_sonnets.generate()
+        sonnet_text = list()
+        for st in sonnet:
+            for verse in st:
+                sonnet_text.append({'text': verse['text'], 'meta': format_meta(verse['meta'])})
+            sonnet_text.append({'text': "\n", 'meta': ''})
+        res = {'text': sonnet_text, 'date': get_date_time()}
+        return jsonify(res)
+
+@api.route("/new-html")
+@api.doc(params={'schema': 'The name of a schema (i.e. sonnet_francais)'})
+class NewHtml(Resource):
+    @api.doc('Returns a new sonnet in HTML')
+    def get(self):
+        param_schema = request.args.get('schema', None)
+        if param_schema in schemas:
+            sonnet = generation_sonnets.generate(schemas[param_schema])
+        else:
+            sonnet = generation_sonnets.generate()
         sonnet = generation_sonnets.generate()
-    sonnet = generation_sonnets.generate()
-    sonnet_html = '<div id="sonnet">'
-    for st in sonnet:
-        sonnet_html += "<p>"
-        for verse in st:
-            sonnet_html += f"<span title='{format_meta(verse['meta'])}'>{verse['text']}</span><br/>"
-        sonnet_html += "</p>"
-    sonnet_html += '</div>'
-    date = f"<small>{get_date_time()}</small>"
-    return sonnet_html+date
+        sonnet_html = '<div id="sonnet">'
+        for st in sonnet:
+            sonnet_html += "<p>"
+            for verse in st:
+                sonnet_html += f"<span title='{format_meta(verse['meta'])}'>{verse['text']}</span><br/>"
+            sonnet_html += "</p>"
+        sonnet_html += '</div>'
+        date = f"<small>{get_date_time()}</small>"
+        return sonnet_html+date
 
 def get_date_time():
     now = datetime.datetime.now()
