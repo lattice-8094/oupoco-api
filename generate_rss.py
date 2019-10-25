f26cc73eb95af9af64d5161e174c934c52b00c3f
Clément Plancq <clement.plancq@ens.fr>
2019-10-25 12:36:22 +0200

    ajout timezone

--- generate_rss.py
+++ generate_rss.py
@@ -7,17 +7,23 @@ import schedule
 import time
 import datetime
 import pytz
+import re
 from feedgen.feed import FeedGenerator
 import generation_sonnets
 
-rss_file = "rss/rss.xml"
-fg = FeedGenerator()
-fg.title('Oupoco feed')
-fg.subtitle('Une strophe par minute, un sonnet toutes les 5 minutes')
-fg.link( href='http://apps.lattice.cnrs.fr/oupoco-rss/rss.xml', rel='self' )
-fg.language('fr')
-fg.rss_file(rss_file, extensions=False, pretty=True)
+RSS_FILE = "rss/rss.xml"
+timezone = pytz.timezone("Europe/Paris")
 
+
+def write_rss_feed(rss_str, rss_file=RSS_FILE):
+    """
+    Dirty hack to remove namespace declaration and atom:link element which hang on apps.lattice.cnrs.fr
+    """
+    rss_str = rss_str.decode("utf-8")
+    rss_str = re.sub(r"<rss.+?>", "<rss>", rss_str)
+    rss_str = re.sub(r"<atom:link.+?>", "", rss_str)
+    with open(rss_file, 'w') as out:
+        out.write(rss_str)
 
 def job():
     schema=('ABAB','ABAB','CCD','EDE')
@@ -27,18 +33,30 @@ def job():
         for st in sonnet:   
             for verse in st:
                 stanza += verse['text']
-                stanza += "<lb/>"
+                stanza += "\n"
         fe = fg.add_entry()
         if len(st_schema) == 4:
             fe.title('Quatrain')
         else:
             fe.title('Tercet')
         fe.description(stanza)
-        fe.pubDate(pytz.utc.localize(datetime.datetime.now()))
-        #rssfeed  = fg.rss_str(pretty=True)
+        fe.pubDate(timezone.localize(datetime.datetime.now()))
+        rssfeed = fg.rss_str(pretty=True)
         #print(rssfeed)
-        fg.rss_file(rss_file, pretty=True)
+        write_rss_feed(rssfeed)
+        #fg.rss_file(rss_file, pretty=True)
         time.sleep(60)
+
+
+fg = FeedGenerator()
+fg.title('Oupoco feed')
+fg.subtitle('Une strophe par minute, un sonnet toutes les 5 minutes')
+fg.link( href='http://apps.lattice.cnrs.fr/oupoco-rss/rss.xml', rel='self' )
+fg.language('fr')
+fg.lastBuildDate(timezone.localize(datetime.datetime.now()))
+rssfeed = fg.rss_str(pretty=True)
+write_rss_feed(rssfeed)
+
 
 schedule.every(5).minutes.do(job)
 #schedule.every().hour.do(job)
