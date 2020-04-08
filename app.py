95d4faff9115cb1a6e74a0605365ad674e9c67e4
Clément Plancq <clement.plancq@ens.fr>
2020-04-08 17:37:57 +0200

    correction du vers de Sainte-Beuve s-517-1-1 + renommage schémas (suppression 'schema_')

--- app.py
+++ app.py
@@ -75,17 +75,17 @@ authors = generation_sonnets.get_authors()
 themes = generation_sonnets.get_themes()
 dates = generation_sonnets.get_dates()
 schemas = {
-    "sonnet_sicilien1": ("ABAB", "ABAB", "CDE", "CDE"),
-    "sonnet_sicilien2": ("ABAB", "ABAB", "CDC", "CDC"),
-    "sonnet_petrarquien1": ("ABBA", "ABBA", "CDE", "CDE"),
-    "sonnet_petrarquien2": ("ABBA", "ABBA", "CDC", "DCD"),
-    "sonnet_petrarquien3": ("ABBA", "ABBA", "CDE", "DCE"),
-    "sonnet_marotique": ("ABBA", "ABBA", "CCD", "EED"),
-    "sonnet_francais": ("ABBA", "ABBA", "CCD", "EDE"),
-    "sonnet_queneau": ("ABAB", "ABAB", "CCD", "EDE"),
-    "sonnet_shakespearien": ("ABAB", "CDCD", "EFEF", "GG"),
-    "sonnet_spencerien": ("ABAB", "BCBC", "CDCD", "EE"),
-    "sonnet_irrationnel": ("AAB", "C", "BAAB", "C", "CDCCD"),
+    "sicilien1": ("ABAB", "ABAB", "CDE", "CDE"),
+    "sicilien2": ("ABAB", "ABAB", "CDC", "CDC"),
+    "petrarquien1": ("ABBA", "ABBA", "CDE", "CDE"),
+    "petrarquien2": ("ABBA", "ABBA", "CDC", "DCD"),
+    "petrarquien3": ("ABBA", "ABBA", "CDE", "DCE"),
+    "marotique": ("ABBA", "ABBA", "CCD", "EED"),
+    "francais": ("ABBA", "ABBA", "CCD", "EDE"),
+    "queneau": ("ABAB", "ABAB", "CCD", "EDE"),
+    "shakespearien": ("ABAB", "CDCD", "EFEF", "GG"),
+    "spencerien": ("ABAB", "BCBC", "CDCD", "EE"),
+    "irrationnel": ("AAB", "C", "BAAB", "C", "CDCCD"),
 }
 
 @api.route("/schemas")
