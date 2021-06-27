0daa47a3dc34937f85570dcd57890a9a91662212
Clément Plancq <clement.plancq@ens.fr>
2021-06-27 23:51:43 +0200

    docker oupoco-rss fonctionne avec api

--- Dockerfile
+++ Dockerfile
@@ -10,9 +10,6 @@ WORKDIR /code
 COPY requirements_rss.txt /code/requirements.txt
 RUN pip install -r requirements.txt
 
-COPY generation_sonnets.py /code/generation_sonnets.py
-COPY bd_meta.json /code/bd_meta.json
-COPY bd_rimes.json /code/bd_rimes.json
 COPY generate_rss.py /code/generate_rss.py
 COPY generate_serve_rss.sh /code/generate_serve_rss.sh
 ENTRYPOINT ["./generate_serve_rss.sh"]
--- generate_rss.py
+++ generate_rss.py
@@ -1,6 +1,6 @@
 """
 Générateur de fichier rss
-1 strophe par minute
+1 sonnet par minute
 """
 
 import schedule
@@ -9,11 +9,11 @@ import datetime
 import pytz
 import re
 import requests
-from lxml import etree
+import json
 from feedgen.feed import FeedGenerator
-import generation_sonnets
 
 RSS_FILE = "rss/rss.xml"
+OUPOCO_CALL = "http://oupoco_api/new"
 timezone = pytz.timezone("Europe/Paris")
 
 
@@ -24,63 +24,56 @@ def write_rss_feed(rss_str, rss_file=RSS_FILE):
     rss_str = rss_str.decode("utf-8")
     rss_str = re.sub(r"<rss.+?>", "<rss>", rss_str)
     rss_str = re.sub(r"<atom:link.+?>", "", rss_str)
-    with open(rss_file, 'w') as out:
+    with open(rss_file, "w") as out:
         out.write(rss_str)
+
 
 def restart():
     """
     start a new rss file
     """
     fg = FeedGenerator()
-    fg.title('Oupoco feed')
-    fg.subtitle('Une strophe par minute, un sonnet toutes les 5 minutes')
-    fg.link( href='https://apps.lattice.cnrs.fr/oupoco_rss/rss.xml', rel='self' )
-    fg.language('fr')
+    fg.title("Oupoco feed")
+    fg.subtitle("Une strophe par minute")
+    fg.link(href="https://apps.lattice.cnrs.fr/oupoco_rss/rss.xml", rel="self")
+    fg.language("fr")
     fg.lastBuildDate(timezone.localize(datetime.datetime.now()))
     rssfeed = fg.rss_str(pretty=True)
     write_rss_feed(rssfeed)
 
 
 def job():
-    schema=('ABAB','ABAB','CCD','EDE')
-    for st_schema in schema:
-        stanza = ""
-        sonnet = generation_sonnets.generate(schema=tuple(st_schema))
-        for st in sonnet:   
-            for verse in st:
-                stanza += verse['text']
-                stanza += "\n"
+    schema = ("ABAB", "ABAB", "CCD", "EDE")
+    resp = requests.get(OUPOCO_CALL)
+    sonnet_json = json.loads(resp.content)
+    for item in sonnet_json["text"]:
         fe = fg.add_entry()
-        if len(st_schema) == 4:
-            fe.title('Quatrain')
-        else:
-            fe.title('Tercet')
-        fe.description(stanza)
+        fe.title(item["meta"])
+        fe.description(item["text"])
         fe.pubDate(timezone.localize(datetime.datetime.now()))
-        rssfeed = fg.rss_str(pretty=True)
-        #print(rssfeed)
-        write_rss_feed(rssfeed)
-        #fg.rss_file(rss_file, pretty=True)
-        time.sleep(60)
+    rssfeed = fg.rss_str(pretty=True)
+    write_rss_feed(rssfeed)
+    # fg.rss_file(rss_file, pretty=True)
+    time.sleep(60)
 
 
 fg = FeedGenerator()
-fg.title('Oupoco feed')
-fg.subtitle('Une strophe par minute, un sonnet toutes les 5 minutes')
-fg.link( href='https://apps.lattice.cnrs.fr/oupoco_rss/rss.xml', rel='self' )
-fg.language('fr')
+fg.title("Oupoco feed")
+fg.subtitle("Une strophe par minute")
+fg.link(href="https://apps.lattice.cnrs.fr/oupoco_rss/rss.xml", rel="self")
+fg.language("fr")
 fg.lastBuildDate(timezone.localize(datetime.datetime.now()))
 rssfeed = fg.rss_str(pretty=True)
 write_rss_feed(rssfeed)
 
 
-schedule.every(5).minutes.do(job)
+schedule.every(1).minutes.do(job)
 schedule.every().monday.do(restart)
-#schedule.every().hour.do(job)
-#schedule.every().day.at("10:30").do(job)
-#schedule.every(5).to(10).minutes.do(job)
-#schedule.every().wednesday.at("13:15").do(job)
-#schedule.every().minute.at(":17").do(job)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every(5).to(10).minutes.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+# schedule.every().minute.at(":17").do(job)
 
 while True:
     schedule.run_pending()
